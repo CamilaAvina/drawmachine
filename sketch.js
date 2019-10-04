6abd77f04d40a1151c79b50382ab94b4a2fca784
Camila Avina <twin2avina@gmail.com>
2019-10-03 17:50:37 -0700

    improving drawing machine

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+let array = [];
+
+
+
 function setup() {
   createCanvas(600, 600);
   background(220);
   strokeWeight(5);
+  noFill();
 }
 
 function draw() {
@@ -9,21 +14,33 @@ function draw() {
 
 
   if (mouseIsPressed) {
-    stroke(map(mouseX,0,600,0,255,true));
-    line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
-    line(mouseX, mouseY, pmouseX, pmouseY);
+  //  stroke(map(mouseX, 0, 600, 0, 255, true));
+    //  line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    array.push([mouseX, mouseY]);
+
   }
-
-
-
 
 }
-function keyTyped(){
+
+function keyTyped() {
 
 
-  if (key==='s'){
+  if (key === 's') {
     //save this image
-    saveCanvas('Drawing Machine','png');
-  }
-  return false;
+    saveCanvas('Drawing Machine', 'png');
+  } else if (key === 'd') {
+    //display image
+    //console.log(array[0]);
+    //  console.log(array[0][1]);
+  beginShape();
+    for (let i = 0; i < array.length -1; i++) {
+      //line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
+
+      curveVertex(array[i][0], array[i][1]);
+      }
+  endShape();
+
+}
+return false;
 }
