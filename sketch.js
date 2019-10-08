3e941bc1d23d0a6f34ae18a34b8dcad6ca01adce
Camila Avina <twin2avina@gmail.com>
2019-10-07 17:26:30 -0700

    more ro drawing machine

--- sketch.js
+++ sketch.js
@@ -1,27 +1,41 @@
-let array = [];
 
+
+
+let array = [];
+let backgroundColor = 200;
 
 
 function setup() {
-  createCanvas(600, 600);
-  background(220);
+  createCanvas(windowWidth, windowHeight);
+  background(backgroundColor);
   strokeWeight(5);
   noFill();
 }
 
 function draw() {
 
+//  background(200,50,133,5);
+  if (mouseIsPressed){
+//  stroke(map(mouseX, 0, 600, 0, 255, true));
+//  line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
+//  line(mouseX, mouseY, pmouseX, pmouseY);
+  backgroundColor -= 3;
+  background(backgroundColor);
+  //line(mouseX, mouseY, pmouseX, pmouseY);
 
+  array.push([mouseX, mouseY]);
+  beginShape();
+  for (let i = 0; i < array.length; i++) {
+    //line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
 
-  if (mouseIsPressed) {
-  //  stroke(map(mouseX, 0, 600, 0, 255, true));
-    //  line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
-    //line(mouseX, mouseY, pmouseX, pmouseY);
-    array.push([mouseX, mouseY]);
-
+    curveVertex(array[i][0], array[i][1]);
   }
+  endShape();
 
 }
+}
+
+
 
 function keyTyped() {
 
@@ -30,17 +44,28 @@ function keyTyped() {
     //save this image
     saveCanvas('Drawing Machine', 'png');
   } else if (key === 'd') {
+    background(255);
     //display image
-    //console.log(array[0]);
-    //  console.log(array[0][1]);
-  beginShape();
-    for (let i = 0; i < array.length -1; i++) {
+
+
+
+    beginShape();
+    for (let i = 0; i < array.length; i++) {
       //line(array[i][0], array[i][1], array[i+1][0], array[i+1][1]);
 
       curveVertex(array[i][0], array[i][1]);
-      }
-  endShape();
+    }
+    endShape();
 
+  }
+
+  return false;
 }
-return false;
+
+function mousePressed() {
+  array = [];
+  backgroundColor = 255;
+
+
+
 }
